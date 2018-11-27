9c14c4b89dfe04e5c471386c8628d78b7b542d91
Dennygod <dennygod1220@gmail.com>
2018-11-27 17:56:55 +0800

    1127_6

--- app/Controllers/Http/WelcomeController.js
+++ app/Controllers/Http/WelcomeController.js
@@ -1,12 +1,19 @@
 'use strict'
 var fs = use('fs');
+var FC = require('../../../cusmodules/FileControl');
 class WelcomeController {
   async index({view,auth}) {
+    var DemoPage_site;
+    var project_file;
     var username = auth.user.username.trim();
-    var project_file = fs.readdirSync('public/UserProfile/' + username + '/Project');
-    var DemoPage_site = fs.readdirSync('public/UserProfile/' + username + '/DemoPage');
-    console.log(DemoPage_site);
-    
+    if(FC.Exists('public/UserProfile/' + username + '/Project') == true){
+        project_file = fs.readdirSync('public/UserProfile/' + username + '/Project');
+    }
+    if(FC.Exists('public/UserProfile/' + username + '/DemoPage') == true){
+        project_file = fs.readdirSync('public/UserProfile/' + username + '/DemoPage');
+    }
+        
+
     return view.render('welcome', {
       project_file: project_file,
       DemoPage_site:DemoPage_site
