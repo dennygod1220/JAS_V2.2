590dcb1aeb1d78c2e71659cccaf5281fc122c914
Dennygod <dennygod1220@gmail.com>
2018-11-28 09:30:04 +0800

    hate basket ball

--- cusmodules/ZoneChange/basic/ballgametime_m_44.js
+++ cusmodules/ZoneChange/basic/ballgametime_m_44.js
@@ -3,7 +3,6 @@ module.exports = async (htmlcode) => {
     var $ = cheerio.load(htmlcode);
   
  
-  var b = $('#article_view_body > div.main_block > div > div > div.container-fluid.view_main > div > div.col.ctnBox > div.content.htmlview > article > div.ad_box');
-$(b).removeAttr('class');
+  var b = $('#endText>p>.posts-cjtz.content-cjtz-mini.clearfix')[0];
 $(b).children().remove();
 $(b).append('<ins class="clickforceads" style="display:inline-block;width:4px;height:4px;" data-ad-zone="8730"></ins><script async type="text/javascript" src="//cdn.doublemax.net/js/init.js"></script>');return $.html();}
--- cusmodules/ZoneChange/custom/bnext_m_44.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (htmlcode,zone) => {
-  var cheerio = require('cheerio');
-  var $ = cheerio.load(htmlcode);
-  var b = $('#article_view_body > div.main_block > div > div > div.container-fluid.view_main > div > div.col.ctnBox > div.content.htmlview > article > div.ad_box');
-$(b).removeAttr('class');
-$(b).children().remove();
-$(b).append(zone);return $.html();}
