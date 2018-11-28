e1ff51a4e2e08e8efe29a41c5e2815ed30f16b29
Dennygod <dennygod1220@gmail.com>
2018-11-28 08:57:02 +0800

    1128 bnext

--- cusmodules/ZoneChange/basic/bnext_m_44.js
+++ cusmodules/ZoneChange/basic/bnext_m_44.js
@@ -3,7 +3,8 @@ module.exports = async (htmlcode) => {
     var $ = cheerio.load(htmlcode);
   
  
-  var b = $('#div-gpt-ad-1502440076374-0');
+  var b = $('#ad-view-in-article-2nd_7_0');
+  $(b).removeAttr('data-pos');
 $(b).removeAttr('class');
 $(b).children().remove();
 $(b).append('<ins class="clickforceads" style="display:inline-block;width:4px;height:4px;" data-ad-zone="8730"></ins><script async type="text/javascript" src="//cdn.doublemax.net/js/init.js"></script>');return $.html();}
--- cusmodules/ZoneChange/custom/bnext_m_44.js
+++ cusmodules/ZoneChange/custom/bnext_m_44.js
@@ -1,6 +1,8 @@
 module.exports = async (htmlcode,zone) => {
   var cheerio = require('cheerio');
-  var $ = cheerio.load(htmlcode);var b = $('#div-gpt-ad-1502440076374-0');
+  var $ = cheerio.load(htmlcode);
+  var b = $('#ad-view-in-article-2nd_7_0');
+  $(b).removeAttr('data-pos');
 $(b).removeAttr('class');
 $(b).children().remove();
 $(b).append(zone);return $.html();}
