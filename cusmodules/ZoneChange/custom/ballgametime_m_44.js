77d98a62a4a2204b4fa28e4f00e456a73413ece7
Dennygod <dennygod1220@gmail.com>
2018-11-29 11:42:21 +0800

    1129

--- cusmodules/ZoneChange/custom/ballgametime_m_44.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (htmlcode,zone) => {
-  var cheerio = require('cheerio');
-  var $ = cheerio.load(htmlcode);var b = $('#endText>p>.posts-cjtz.content-cjtz-mini.clearfix')[0];
-$(b).children().remove();
-$(b).append(zone);return $.html();}
